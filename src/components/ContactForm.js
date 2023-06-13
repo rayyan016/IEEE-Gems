cc818a2eba728b5912f9d082496e664b24b86132
Rayyan <rayyanaslam16@gmail.com>
2023-06-14 02:06:03 +0530

    Added background colour

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -85,127 +85,132 @@ const ContactForm = () => {
   };
 
   return (
-    <form className="max-w-md mx-auto p-4" onSubmit={handleSubmit}>
-      <label className="block mb-2">
-        <span className="text-gray-700">Name:</span>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="block w-full border border-gray-300 rounded p-2 mt-1"
-          required
-        />
-      </label>
-
-      <label className="block mb-2">
-        <span className="text-gray-700">Email:</span>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          className="block w-full border border-gray-300 rounded p-2 mt-1"
-          required
-        />
-      </label>
-
-      <label className="block mb-2">
-        <span className="text-gray-700">Phone Number:</span>
-        <input
-          type="tel"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          className="block w-full border border-gray-300 rounded p-2 mt-1"
-          required
-        />
-      </label>
-
-      <label className="block mb-2">
-        <span className="text-gray-700">How Can We Help? (Description):</span>
-        <textarea
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
-          className="block w-full border border-gray-300 rounded p-2 mt-1"
-          required
-        />
-      </label>
-
-      <label className="block mb-2">
-        <span className="text-gray-700">Services:</span>
-        <div className="mt-1">
-          <label className="inline-flex items-center">
-            <input
-              type="checkbox"
-              name="services"
-              value="Website Design"
-              onChange={handleChange}
-              className="form-checkbox h-5 w-5 text-blue-500"
-            />
-            <span className="ml-2">Website Design</span>
-          </label>
-          <label className="inline-flex items-center ml-6">
-            <input
-              type="checkbox"
-              name="services"
-              value="Content Creation"
-              onChange={handleChange}
-              className="form-checkbox h-5 w-5 text-blue-500"
-            />
-            <span className="ml-2">Content Creation</span>
-          </label>
-          <label className="inline-flex items-center ml-6">
-            <input
-              type="checkbox"
-              name="services"
-              value="UX Design"
-              onChange={handleChange}
-              className="form-checkbox h-5 w-5 text-blue-500"
-            />
-            <span className="ml-2">UX Design</span>
-          </label>
-          <label className="inline-flex items-center">
-            <input
-              type="checkbox"
-              name="services"
-              value="Strategy & Consulting"
-              onChange={handleChange}
-              className="form-checkbox h-5 w-5 text-blue-500"
-            />
-            <span className="ml-2">Strategy & Consulting</span>
-          </label>
-          <label className="inline-flex items-center">
-            <input
-              type="checkbox"
-              name="services"
-              value="User Research"
-              onChange={handleChange}
-              className="form-checkbox h-5 w-5 text-blue-500"
-            />
-            <span className="ml-2">User Research</span>
-          </label>
-          <label className="inline-flex items-center">
-            <input
-              type="checkbox"
-              name="services"
-              value="Other"
-              onChange={handleChange}
-              className="form-checkbox h-5 w-5 text-blue-500"
-            />
-            <span className="ml-2">Other</span>
-          </label>
-        </div>
-      </label>
-
-      <button
-        type="submit"
-        className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition-colors"
+    <div className="bg-slate-800">
+      <form
+        className="max-w-md mx-auto p-4 contact-form-content"
+        onSubmit={handleSubmit}
       >
-        Send Message
-      </button>
-    </form>
+        <label className="block mb-2">
+          <p className="text-gray-300 text-left">Name:</p>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="block w-full border border-gray-300 rounded p-2 mt-1"
+            required
+          />
+        </label>
+
+        <label className="block mb-2">
+          <p className="text-gray-300 text-left">Email:</p>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="block w-full border border-gray-300 rounded p-2 mt-1"
+            required
+          />
+        </label>
+
+        <label className="block mb-2">
+          <p className="text-gray-300 text-left">Phone Number:</p>
+          <input
+            type="tel"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            className="block w-full border border-gray-300 rounded p-2 mt-1"
+            required
+          />
+        </label>
+
+        <label className="block mb-2">
+          <p className="text-gray-300 text-left">How Can We Help? (Description):</p>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            className="block w-full border border-gray-300 rounded p-2 mt-1"
+            required
+          />
+        </label>
+
+        <label className="block mb-2">
+          <p className="text-gray-300 text-left">Services:</p>
+          <div className="mt-1 text-gray-300 grid grid-cols-2 text-sm sm:text-base">
+            <label className="inline-flex items-center mb-2">
+              <input
+                type="checkbox"
+                name="services"
+                value="Website Design"
+                onChange={handleChange}
+                className="form-checkbox h-5 w-5 text-blue-500"
+              />
+              <span className="ml-2">Website Design</span>
+            </label>
+            <label className="inline-flex items-center mb-2">
+              <input
+                type="checkbox"
+                name="services"
+                value="Content Creation"
+                onChange={handleChange}
+                className="form-checkbox h-5 w-5 text-blue-500"
+              />
+              <span className="ml-2">Content Creation</span>
+            </label>
+            <label className="inline-flex items-center mb-2">
+              <input
+                type="checkbox"
+                name="services"
+                value="UX Design"
+                onChange={handleChange}
+                className="form-checkbox h-5 w-5 text-blue-500"
+              />
+              <span className="ml-2">UX Design</span>
+            </label>
+            <label className="inline-flex items-center mb-2">
+              <input
+                type="checkbox"
+                name="services"
+                value="Strategy & Consulting"
+                onChange={handleChange}
+                className="form-checkbox h-5 w-5 text-blue-500"
+              />
+              <span className="ml-2">Strategy & Consulting</span>
+            </label>
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="services"
+                value="User Research"
+                onChange={handleChange}
+                className="form-checkbox h-5 w-5 text-blue-500"
+              />
+              <span className="ml-2">User Research</span>
+            </label>
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="services"
+                value="Other"
+                onChange={handleChange}
+                className="form-checkbox h-5 w-5 text-blue-500"
+              />
+              <span className="ml-2">Other</span>
+            </label>
+          </div>
+        </label>
+
+        <button
+          type="submit"
+          className="bg-slate-600 text-white px-24 py-2 rounded hover:bg-slate-700 transition-colors mt-2"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
   );
 };
 
